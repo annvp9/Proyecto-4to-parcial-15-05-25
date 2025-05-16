3986bd90d73cb7045dd0cf66160895fddda2b81f
Ruiz Chimal Jose Maria <22031392@itcelaya.edu.mx>
2025-05-15 23:53:48 -0600

    Primer commit

---
+++ src/util/SHA1Util.java
@@ -0,0 +1,19 @@
+package util;
+
+import java.security.MessageDigest;
+
+public class SHA1Util {
+    public static String encrypt(String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+            byte[] result = md.digest(input.getBytes());
+            StringBuilder sb = new StringBuilder();
+            for (byte b : result) {
+                sb.append(String.format("%02x", b));
+            }
+            return sb.toString();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
